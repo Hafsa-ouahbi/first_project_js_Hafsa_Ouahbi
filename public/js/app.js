1cc588c74cb951dc4930a093132e26bcf010e255
Hafsa Ouahbi <hafsaouahbi52@gmail.com>
2025-12-29 14:29:20 +0100

    updates for the name part

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,54 @@
+class Person {
+    constructor(name, age, password, email) {
+        this.name = name
+        this.age = age
+        this.password = password
+        this.email = email
+    }
+}
 let Answer = prompt("choose one : signing up / logging in / changing the password").trim().toLocaleLowerCase()
+let dataBase = []
 if (Answer == "signing up") {
-    let dataBase = []
-    //*--------------------------name
-    let name = prompt("insert a valid name").trim().toLocaleLowerCase()
+    //*----------------------------------name---------
+    let name ;
+    while(true){// to keep asking until the name is valid
+    name = prompt("insert a valid name :").trim().toLocaleLowerCase()
     let count = 0
     for (let i = 0; i < name.length; i++) {
-        if (name[i] != " " && name[i] != "  ") {
+        if (name[i] !== " ") {
             count++
         }
     }
     if (count < 5) {
-        console.log("please insert a longer name")
-        return
+        alert("please insert a longer name")
+        continue //ask again
     }
+
     let j = 0
-    while(name[j]){
-        if (!((name[j] >= 'a' && name[j] <= 'z') || (name[j] >= 'A' && name[j] <= 'Z'))){
-            console.log("you can't have a nuimbers or special characters in your name please insert a valid name")
-            return
+    let invalid = false //nftardo rah password shih yaeni machi invalid wlkin valis 
+    while (name[j]) {
+        if (!((name[j] >= 'a' && name[j] <= 'z') || (name[j] >= 'A' && name[j] <= 'Z') || name[j] == " ")) {
+            alert("you can't have a nuimbers or special characters in your name please insert a valid name")
+            invalid = true // db bsah rah invalid
+            break
         }
+        j++
     }
-    name = name.charAt(0).toUpperCase() + name.slice(1)//to capitalize the first letter and keep the rest lowercase
+    if(invalid)continue // if invalid is true starts again
     let words = name.split(" ")// to separate the words ila kano endna ktr mn kelma wehda
     let finalres = []
     for (let i = 0; i < words.length; i++) {
         if (words[i] != "") { // if the user taps a multiple spaces it's never enter in this condioion 
-            finalres.push(words[i].charAt(0).toLocaleUpperCase() + words[i].slice(1))
+            finalres.push(words[i][0].toLocaleUpperCase() + words[i].slice(1))
         }
     }
     name = finalres.join(" ")
-    //*------------------------------------email
+    break //ila kan kolchy howa hadak kanbrikiw loup
+}
+    // let newPerson = new Person(name, email, password);
+    // dataBase.push(name)
+    // console.log(dataBase)
+    //*------------------------------------email---------
+    let email = prompt("insert a valid email :").trim().toLowerCase()
+    
 }
